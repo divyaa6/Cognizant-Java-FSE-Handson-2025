d09632d494907d7697f19265de3f3d14d462e177
divyaa6 <divyasri242004@gmail.com>
2025-07-26 16:40:28 +0530

    Week 6 updated

---
+++ Week 6/Exercise 3/Code/scorecalculatorapp/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculateScore name="Ananya" school="Green Valley High School" total={450} goal={5} />
+    </div>
+  );
+}
+
+export default App;
